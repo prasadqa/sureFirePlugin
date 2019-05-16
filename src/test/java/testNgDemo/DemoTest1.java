5f45c36890487458c5c9c5d34a7854da8a29cfbc
prasadqa <challaprasad772723@gmail.com>
2019-05-16 12:27:24 +0530

    first code

---
+++ src/test/java/testNgDemo/DemoTest1.java
@@ -0,0 +1,24 @@
+package testNgDemo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+
+	@Test
+	public void TestOne()
+	{
+		Assert.assertTrue(true);
+	}
+	@Test
+	public void TestTwo()
+	{
+		Assert.assertTrue(false);
+	}
+	@Test(dependsOnMethods="TestTwo")
+	public void TestThird()
+	{
+		Assert.assertTrue(true);
+	}
+	
+}
